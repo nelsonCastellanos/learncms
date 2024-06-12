461daf8b628bd057c18f69c11622c8be8e9ae657
ncastelanos <nelson.castellanos@mercadolibre.com.co>
2024-06-12 13:01:16 -0500

    feat: update with develop

--- frontend/src/styles/global.js
+++ frontend/src/styles/global.js
@@ -2,8 +2,11 @@ import {css, unsafeCSS} from 'lit';
 import containerStyles from './container_styles.css?inline';
 import flexStyles from './flex_styles.css?inline';
 import responsiveUtilities from './responsive_utilities.css?inline';
+import colorStyles from './color_styles.css?inline';
+
 
 export const gridStyles = [
+    unsafeCSS(colorStyles),
     unsafeCSS(containerStyles),
     unsafeCSS(flexStyles),
     unsafeCSS(responsiveUtilities),
